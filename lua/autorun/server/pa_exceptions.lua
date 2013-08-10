c2065d3169e21de196cc085a2daa67a1f1462070
shadowscion <khaotix2@gmail.com>
2013-08-09 20:27:35 -0500

    train tracks

--- lua/autorun/server/pa_exceptions.lua
+++ lua/autorun/server/pa_exceptions.lua
@@ -1,4 +1,3 @@
-// Making sure those pesky wheels and lights mirror properly
 list.Set( "PA_mirror_exceptions", "models/sprops/trans/wheel_d", Angle(180,180,0) )
 list.Set( "PA_mirror_exceptions", "models/sprops/trans/train", Angle(180,180,0) )
 list.Set( "PA_mirror_exceptions", "models/sprops/trans/lights", Angle(0,180,180) )
@@ -6,3 +5,5 @@ list.Set( "PA_mirror_exceptions", "models/sprops/trans/lights", Angle(0,180,180)
 list.Set( "PA_mirror_exceptions_specific", "models/sprops/trans/misc/gauge_1.mdl", Angle(0,180,180) )
 list.Set( "PA_mirror_exceptions_specific", "models/sprops/trans/misc/gauge_2.mdl", Angle(0,180,180) )
 list.Set( "PA_mirror_exceptions_specific", "models/sprops/trans/misc/gauge_3.mdl", Angle(0,180,180) )
+list.Set( "PA_mirror_exceptions_specific", "models/sprops/trans/train/track_t90_01.mdl", Angle(180,90,0) )
+list.Set( "PA_mirror_exceptions_specific", "models/sprops/trans/train/track_t90_02.mdl", Angle(180,90,0) )
